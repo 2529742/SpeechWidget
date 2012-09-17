e96d1b4ac3b3ffc724286ba92bd20a99acded516
Andrey <hello.andrey@gmail.com>
2012-09-17 10:09:06 +0200

    initialized demo files

---
+++ vie.widget.speechwidget.js
@@ -0,0 +1,17 @@
+(function($, undefined) {
+    $.widget('view.vieSpeechWidget', {
+		 _create: function () {
+		 
+		 },
+		 
+		 _init: function(){
+		 
+		 },
+		 options: {
+			vie: undefined,
+			eventHandler: undefined,
+			entityHandler: undefined,
+			options: {}
+		}
+	});
+})(jQuery);
