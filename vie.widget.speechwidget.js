5b3610a847d19c22d13a17d9bd05d68df2d5587b
Andrey <hello.andrey@gmail.com>
2012-09-24 09:59:31 +0200

    fixed spokenOutput - added an element to output speech

--- vie.widget.speechwidget.js
+++ vie.widget.speechwidget.js
@@ -11,8 +11,11 @@
 				self.dialogueManager(results, val);
 			};
 			googleVoice.attr('x-webkit-speech','');
+			
+			var spokenOutput = $('<audio id="spokenOutput">');
 			var dialog = $('<div id="speechWidgetDialog">')
 			.append(googleVoice)
+			.append(spokenOutput)
 			.dialog({
 				title: 'Start annotation'
 			});
@@ -97,6 +100,7 @@
 			var maryString = "process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=" + speak[randomIndex] + "&OUTPUT_TEXT=&effect_Volume_selected=&effect_Volume_parameters=amount%3A2.0%3B&effect_Volume_default=Default&effect_Volume_help=Help&effect_TractScaler_selected=&effect_TractScaler_parameters=amount%3A1.5%3B&effect_TractScaler_default=Default&effect_TractScaler_help=Help&effect_F0Scale_selected=&effect_F0Scale_parameters=f0Scale%3A2.0%3B&effect_F0Scale_default=Default&effect_F0Scale_help=Help&effect_F0Add_selected=&effect_F0Add_parameters=f0Add%3A50.0%3B&effect_F0Add_default=Default&effect_F0Add_help=Help&effect_Rate_selected=&effect_Rate_parameters=durScale%3A1.5%3B&effect_Rate_default=Default&effect_Rate_help=Help&effect_Robot_selected=&effect_Robot_parameters=amount%3A100.0%3B&effect_Robot_default=Default&effect_Robot_help=Help&effect_Whisper_selected=&effect_Whisper_parameters=amount%3A100.0%3B&effect_Whisper_default=Default&effect_Whisper_help=Help&effect_Stadium_selected=&effect_Stadium_parameters=amount%3A100.0&effect_Stadium_default=Default&effect_Stadium_help=Help&effect_Chorus_selected=&effect_Chorus_parameters=delay1%3A466%3Bamp1%3A0.54%3Bdelay2%3A600%3Bamp2%3A-0.10%3Bdelay3%3A250%3Bamp3%3A0.30&effect_Chorus_default=Default&effect_Chorus_help=Help&effect_FIRFilter_selected=&effect_FIRFilter_parameters=type%3A3%3Bfc1%3A500.0%3Bfc2%3A2000.0&effect_FIRFilter_default=Default&effect_FIRFilter_help=Help&effect_JetPilot_selected=&effect_JetPilot_parameters=&effect_JetPilot_default=Default&effect_JetPilot_help=Help&HELP_TEXT=&exampleTexts=I'm%20Spike.&VOICE_SELECTIONS=dfki-spike%20en_GB%20male%20unitselection%20general&AUDIO_OUT=WAVE_FILE&LOCALE=en_GB&VOICE=dfki-spike&AUDIO=WAVE_FILE";
 
 			//load the audio element with the mary TTs web interface url and the mary request & play the audio file
+			var spokenOutput = document.getElementById('spokenOutput');
 			spokenOutput.load();
 			spokenOutput.setAttribute('src', url + maryString);
 			spokenOutput.play();
