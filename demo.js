74dd4577629209e43872af8302ffb7480c2b18e8
Andrey <hello.andrey@gmail.com>
2012-09-24 16:08:02 +0200

    updated widget to send found entities to entityHandler function. modified demo script

--- demo.js
+++ demo.js
@@ -8,14 +8,47 @@ $(function(){
 		var image = $(this);
 		image.vieSpeechWidget({
 			vie: myVIE,
-			entityHandler: function(){
-				annotateImage(this.result,image);
+			entityHandler: function(entities){
+				annotateImage(entities,image);
 			}
 		});
 	});
-	var annotateImage = function(result,image){
-		var url = "http://dbpedia.org/resource/Paul_McCartney";
-		var a = $('<a href="' + url + '" target="_blank">');
-		image.wrap(a);
+	var annotateImage = function(entities,image){
+		var resultsBox = $('<ul id="speechWidgetDialogResultsBox">');
+		$(entities).each(function(){
+			resultsBox.append('<li><input type="radio" name="entity">' + this.getSubjectUri() + '</li>');
+		});
+
+		var radiobox = $('<div>')
+		.append(resultsBox)
+		.dialog({
+			title: 'Please choose the entity',
+			buttons:[
+				{
+					text: "OK",
+					click: function(){
+						var dialog = this;
+						var radioGroup = $('[name="entity"]');
+						$(radioGroup)
+						.each(function(){
+							if(this.checked){
+								var url = this.nextSibling.data;
+								var a = $('<a href="' + url + '" target="_blank">');
+								image.wrap(a);
+								$(dialog).dialog('close');
+							}
+						});
+					}
+				},
+				{
+					text: "Cancel",
+					click: function(){
+						$(this).dialog('close');
+					}
+				}
+			]
+		});
+		
+
 	};
 });
